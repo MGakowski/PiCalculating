549d4d1fe50f20d32e4bbe85e909548a5869cab0
Cyph3r - Matt Gakowski <mgakowski@gmail.com>
2017-01-25 21:40:45 +1000

    Update Gregory-Leibniz.py

--- Gregory-Leibniz.py
+++ Gregory-Leibniz.py
@@ -1,28 +1,32 @@
+from decimal import *
+
 print '[#] The Gregory-Leibniz "infinite" series for calculating Pi!'
 print "[#] Though not very efficient, it will get closer and closer to pi with every iteration, accurately producing " \
       "pi to five decimal places with 500,000 iterations."
 
-pi = float(0.0)
-odd = float(1)
+pi = Decimal(0)
+odd = Decimal(1)
 loop = int(0)
 iterats = int(raw_input("Iterations? "))
 inc = int(raw_input("Increments? "))
 inccheck = int(loop+inc)
+getcontext().prec = int(raw_input("Number of decimal places? "))+1
+
 
 while loop < iterats:
-    
+
     if loop == inccheck:
         print str("*Pi is " + str(pi) + " after " + str(loop) + " iterations.")
         inccheck += inc
-        
+
     pi += 4 / odd
     odd += 2
     loop += 1
-    
+
     if loop == inccheck:
         print str("*Pi is " + str(pi) + " after " + str(loop) + " iterations.")
         inccheck += inc
-        
+
     pi -= 4 / odd
     odd += 2
     loop += 1
